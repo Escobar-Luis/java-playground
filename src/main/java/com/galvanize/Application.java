70be0027cb76ad4072c4a8c53a8e37ed006566a9
Luis Escobar <lae311@stern.nyu.edu>
2022-07-12 15:37:03 -0400

    finished fizzbuzz

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,16 +4,23 @@ public class Application {
 
     public static void main(String[] args){
         if (args.length < 1){
-            System.out.println("Please specify a name and email");
-        }
-        else if (args.length < 2){
-            System.out.println(String.format("Please specify an email for %s",args[0]));
+            System.out.println("Please specify a number");
         }
         else {
-            String firstArgument = args[0];   // arg1
-            String secondArgument = args[1];  // arg2
-            String message = String.format("%s <%s>", firstArgument, secondArgument);
-            System.out.println(message);
+            int number = Integer.parseInt(args[0]);   // arg1
+            if (number % 3 ==0 && number % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (number % 3 ==0){
+                System.out.println("Fizz");
+            }
+            else if (number % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(args[0]);
+                System.out.println(args[0].getClass().getSimpleName());
+            }
         }
     }
 }
